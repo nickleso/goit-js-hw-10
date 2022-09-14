23c9ea0774006433369a587529546f3275cb7967
Nick Leso <nickleso.work@gmail.com>
2022-09-14 21:11:01 +0300

    Create fetch and markup

---
+++ src/fetchCountries.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+// export default class CountriesApiService {
+//   constructor() {
+//     this.searchQuery = '';
+//   }
+// }
+
+function fetchCountries(name) {
+  console.log(name);
+
+  const url = `${BASE_URL}${name}?fields=name,capital,population,flags,languages`;
+  console.log(url);
+  return fetch(url).then(response => response.json());
+}
+
+export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
 import './css/styles.css';
+// import CountriesApiService from './fetchCountries.js';
+import API from './fetchCountries.js';
 
-const DEBOUNCE_DELAY = 300;
+var debounce = require('lodash.debounce');
+const DEBOUNCE_DELAY = 1000;
+
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  listEl: document.querySelector('.country-list'),
+  infoContainerEl: document.querySelector('.country-info'),
+};
+
+console.dir(refs.inputEl);
+console.log(refs.listEl);
+console.log(refs.infoContainerEl);
+
+// const countriesApiService = new CountriesApiService();
+
+const onDebounceSearchCountries = debounce(searchCountries, DEBOUNCE_DELAY);
+refs.inputEl.addEventListener('input', onDebounceSearchCountries);
+
+function searchCountries() {
+  const searchQuery = refs.inputEl.value;
+
+  API.fetchCountries(searchQuery)
+    .then(appendCountriesMarkup)
+    .catch(onFetchError);
+}
+
+// const countriesMarkup = createCountriesMarkup(countries);
+
+function appendCountriesMarkup(country) {
+  refs.infoContainerEl.insertAdjacentHTML(
+    'beforeend',
+    createCountriesMarkup(country)
+  );
+}
+
+// markup function
+function createCountriesMarkup(country) {
+  return country
+    .map(
+      ({
+        flags: { svg },
+        name: { official },
+        capital,
+        population,
+        languages,
+      }) => {
+        return `
+      <div class="coutry-info__wrap">
+        <img src=${svg} alt="flag" width="60"></img>
+        <h2 class="country-info__name">${official}</h2>
+        <p class="country-info__descr">
+          Capital: <span class="country-info__value">${capital}</span>
+        </p>
+        <p class="country-info__descr">
+          Population: <span class="country-info__value">${population}</span>
+        </p>
+        <p class="country-info__descr">
+          Languages: <span class="country-info__value">${languages}</span>
+        </p>
+      </div>`;
+      }
+    )
+    .join('');
+}
+
+function onFetchError() {
+  // body
+}
